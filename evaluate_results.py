b4b57210b76326be253114e016ba99f2ce102215
Wee Tee Soh <weeteesoh345@gmail.com>
2019-05-17 23:48:25 +0800

    17/5/2019 v3

--- evaluate_results.py
+++ evaluate_results.py
@@ -14,6 +14,7 @@ import pickle
 import os
 from text_GCN import gcn
 from sklearn.metrics import confusion_matrix
+import pandas as pd
 
 def load_pickle(filename):
     completeName = os.path.join("./data/",\
@@ -31,9 +32,32 @@ def save_as_pickle(filename, data):
 def evaluate(output, labels_e):
     _, labels = output.max(1); labels = labels.numpy()
     return sum([(e-1) for e in labels_e] == labels)/len(labels)
-        
+
+def misclassified(df_data, pred_labels_test_idx, labels_not_selected, test_idxs, book_dict):
+    for actual, pred, test_idx in zip([(e-1) for e in labels_not_selected], \
+                             list(pred_labels_test_idx.max(1)[1].numpy()),\
+                             test_idxs):
+        if actual != pred:
+            print("Book: %s\nChapter: %s" % (book_dict[actual+1], df_data.loc[test_idx]["c"]))
+            print("Predicted as %s" % book_dict[pred+1]); print("\n")
+    
 if __name__=="__main__":
     base_path = "./data/"
+    ### Loads bible data
+    df = pd.read_csv(os.path.join(base_path,"t_bbe.csv"))
+    df.drop(["id", "v"], axis=1, inplace=True)
+    df = df[["t","c","b"]]
+    ### one chapter per document, labelled by book
+    df_data = pd.DataFrame(columns=["c", "b"])
+    for book in df["b"].unique():
+        dum = pd.DataFrame(columns=["c", "b"])
+        dum["c"] = df[df["b"] == book].groupby("c").apply(lambda x: (" ".join(x["t"])).lower())
+        dum["b"] = book
+        df_data = pd.concat([df_data,dum], ignore_index=True)
+    del df
+    book_dict = pd.read_csv(os.path.join(base_path, "key_english.csv"))
+    book_dict = {book.lower():number for book, number in zip(book_dict["field.1"], book_dict["field"])}
+    book_dict = {v:k for k,v in zip(book_dict.keys(),book_dict.values())}
     ### Loads graph data
     G = load_pickle("text_graph.pkl")
     A = nx.to_numpy_matrix(G, weight="weight"); A = A + np.eye(G.number_of_nodes())
@@ -89,3 +113,5 @@ if __name__=="__main__":
     ax.set_xlabel("Actual class", fontsize=17)
     ax.set_ylabel("Predicted", fontsize=17)
     plt.savefig(os.path.join("./data/", "confusion_matrix.png"))
+    #### Prints misclassified labels
+    misclassified(df_data, pred_labels[test_idxs], labels_not_selected, test_idxs, book_dict)
